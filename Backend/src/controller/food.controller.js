6f8f31625b0453b43859c9dbc1e1f4369d97036c
Pradeep Kumar <vermapradeep3000@gmail.com>
2025-11-11 15:44:49 +0530

    data  set in MogoDb with video file

--- Backend/src/controller/food.controller.js
+++ Backend/src/controller/food.controller.js
@@ -1,9 +1,13 @@
 const foodModel= require("../models/food.model");
 
 
- async function createFood(req,res){
 
-    
+ async function createFood(req,res){
+    console.log(req.foodPartner)
+    console.log(req.body)
+    console.log(req.file)
+
+    res.send("Food item created")
  }
 
 
--- Backend/src/routes/food.routs.js
+++ Backend/src/routes/food.routs.js
@@ -1,15 +1,20 @@
 const express= require("express");
 const foodController=require("../controller/food.controller");
 const authMiddeware=require("../midleware/auth.midleware")
+const multer= require("multer")
+const upload= multer({
+    storage: multer.memoryStorage()
+})
 
 
 
 
 const routes= express.Router()
 
-routes.post("/",authMiddeware.authFoodPartnerMiddeware,foodController.createFood)
+routes.post("/",authMiddeware.authFoodPartnerMiddeware, upload.single("video"),foodController.createFood)
 
 
 
 
 module.exports=routes
+
