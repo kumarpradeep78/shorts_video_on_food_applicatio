31f8f803c6e18128ad955711d47c14e6c5c0ac38
Pradeep Kumar <vermapradeep3000@gmail.com>
2025-11-12 13:02:43 +0530

    before add services imagekit

--- Backend/src/controller/food.controller.js
+++ Backend/src/controller/food.controller.js
@@ -1,14 +1,17 @@
 const foodModel= require("../models/food.model");
+const { v4: uuid } = require('uuid');
 
-
+//const storageService=require("../services/storage.service")
 
  async function createFood(req,res){
     console.log(req.foodPartner)
     console.log(req.body)
     console.log(req.file)
-
+   //  const result= await storageService.uploadFile(req.file.buffer,uuid())
+     //console.log("-------Result---------")
+   // console.log(result)
     res.send("Food item created")
  }
 
 
- module.exports={createFood}
+ module.exports={createFood} 
