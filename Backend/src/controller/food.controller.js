40f0769f83d01c1aa33dd37e8dafa3e7eae66c59
Pradeep Kumar <vermapradeep3000@gmail.com>
2025-11-12 02:44:19 -0500

    image kit service added successfully

--- Backend/src/controller/food.controller.js
+++ Backend/src/controller/food.controller.js
@@ -1,15 +1,15 @@
 const foodModel= require("../models/food.model");
 const { v4: uuid } = require('uuid');
 
-//const storageService=require("../services/storage.service")
+const storageService=require("../services/storage.service")
 
  async function createFood(req,res){
     console.log(req.foodPartner)
     console.log(req.body)
     console.log(req.file)
-   //  const result= await storageService.uploadFile(req.file.buffer,uuid())
-     //console.log("-------Result---------")
-   // console.log(result)
+    const result= await storageService.uploadFile(req.file.buffer.toString('base64'),uuid())
+    console.log("-------Result---------")
+   console.log(result)
     res.send("Food item created")
  }
 
